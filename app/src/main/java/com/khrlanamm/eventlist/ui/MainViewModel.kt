1d4d5dffe43173a5dbf5093088b7772e229a6afb
khrlanamm <anam160404@gmail.com>
2024-11-16 05:00:10 +0700

    Add MainViewModel.kt

---
+++ app/src/main/java/com/khrlanamm/eventlist/ui/MainViewModel.kt
@@ -0,0 +1,27 @@
+package com.khrlanamm.eventlist.ui
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.khrlanamm.eventlist.data.SettingsPreferences
+import com.khrlanamm.eventlist.data.SettingsPreferences.Companion.dataStore
+
+class MainViewModel private constructor(
+    private val settingsPreferences: SettingsPreferences
+) :
+    ViewModel() {
+
+    fun getDarkMode() = settingsPreferences.getDarkMode()
+
+    class Factory(private val context: Context) :
+        ViewModelProvider.NewInstanceFactory() {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return MainViewModel(SettingsPreferences.getInstance(context.dataStore)) as T
+        }
+    }
+
+    companion object {
+        fun getInstance(context: Context) = Factory(context)
+    }
+}
