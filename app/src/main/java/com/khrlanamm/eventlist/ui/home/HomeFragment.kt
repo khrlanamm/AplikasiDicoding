07f32bcdcf8a9d613ad86cc2ecebce36e489fb7a
khrlanamm <anam160404@gmail.com>
2024-11-16 02:22:47 +0700

    Binding switch theme button to fragment home

--- app/src/main/java/com/khrlanamm/eventlist/ui/home/HomeFragment.kt
+++ app/src/main/java/com/khrlanamm/eventlist/ui/home/HomeFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.fragment.app.Fragment
@@ -32,6 +34,18 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val switchTheme = binding.switchTheme
+
+        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchTheme.isChecked = false
+            }
+        }
 
         // Set up RecyclerView and Adapter
         val upcomingEventsAdapter = EventAdapter { event ->
