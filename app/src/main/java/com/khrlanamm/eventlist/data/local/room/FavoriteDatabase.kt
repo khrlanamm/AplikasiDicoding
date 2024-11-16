00df1ce3de2c5e994768b0f08ce97a68a5f53602
khrlanamm <anam160404@gmail.com>
2024-11-16 22:18:45 +0700

    UDAH BENER INSYAALLAH

--- app/src/main/java/com/khrlanamm/eventlist/data/local/room/FavoriteDatabase.kt
+++ app/src/main/java/com/khrlanamm/eventlist/data/local/room/FavoriteDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.khrlanamm.eventlist.data.local.entity.EventDetail
 
-@Database(entities = [EventDetail::class], version = 1, exportSchema = true)
+@Database(entities = [EventDetail::class], version = 1, exportSchema = false)
 abstract class FavoriteDatabase : RoomDatabase() {
     abstract fun favoriteDao(): FavoriteDao
 
@@ -19,7 +19,7 @@ abstract class FavoriteDatabase : RoomDatabase() {
             INSTANCE ?: synchronized(this) {
                 INSTANCE ?: Room.databaseBuilder(
                     context.applicationContext,
-                    FavoriteDatabase::class.java, "sched_database"
+                    FavoriteDatabase::class.java, "local_favorites_data"
                 ).fallbackToDestructiveMigration()
                     .build().also { INSTANCE = it }
             }
